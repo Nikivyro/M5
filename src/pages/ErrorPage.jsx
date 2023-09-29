e1520f1e9d116cbbbbdbd530760d9d349d6bc736
Niko <nikivyro@gmail.com>
2023-09-29 17:40:33 +0200

    Error page layout

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
+import { Alert, Button, Col, Container, Row } from 'react-bootstrap'
 
 export default function ErrorPage() {
   return (
     <MainLayout>
-        <div><p>Ooops la pagina che stai cerando non esiste.</p></div>
-        <Link to='/'>Torna alla home</Link>
+      <Container className='min-vh-100'>
+        <Row className='justify-content-center align-items-center'>
+          <Col>
+            <Alert variant="warning">Ooops la pagina che stai cerando non esiste.</Alert>
+            <Button variant='primary'>
+                <Link to='/' className='text-white text-decoration-none'><i className="bi bi-arrow-left me-1"></i>Torna alla home</Link>
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </MainLayout>
   )
 }
